f7a9c666dd79f625a2c4c4437f2e8b5015004e04
anas13-meet <anas.kiswani2098@gmail.com>
2014-05-04 19:23:26 -0400

    Update First Test.py

--- First Test.py
+++ First Test.py
@@ -15,6 +15,17 @@ class Game(events.Event):
         self._image_loc = None
         self.size = self.width, self.height = 800, 500
         self._background = (96,96,96)
+        self.finishButton = Button.Button(width,length, size, height, (x coord, y coord),none, self)
+     
+    def finishButtonClicked:
+     If MoneyPutinside > totalMoneyNeeded:
+      self.screen= "Main"
+     for itemCat in self._itemCategories:
+      item.amount = 0
+     for item in self._itemCategories:
+      item.amount = 0
+      
+      self.moneyin = 0
  
     
             
@@ -40,11 +51,11 @@ class Game(events.Event):
             self._screen = "itemCatScreen:03"
             
         def itemIncrementCommand(self):
-            print "itemIncrementCommand"
+            print "itemCat0ItemNumber1Command"
             self.incriment()
         
         def itemDecrementCommand(self):
-            print "itemDecrementCommand"
+            print "itemCat0ItemNumber1Command"
             self.decrement()
         
         itemsToAdd = []    
@@ -98,18 +109,6 @@ class Game(events.Event):
         self.backButton = Button.Button(144, 50, (50,50), goBackCommand, None, self)
         self.backButton.currentColor = self.backButton.default_color = self.backButton.hover_color = None #Make sure that the surface is not overwritten
         self.backButton.surface = Label.Label("BACK").getSurface()
-        
-        
-        def goToCheckout(self):
-            self._screen = "checkOut"
-        self.checkoutButton = Button.Button(144, 50, (500,250), goToCheckout, None, self)
-        self.checkoutButton.currentColor = self.checkoutButton.default_color = self.checkoutButton.hover_color = None #Make sure that the surface is not overwritten
-        self.checkoutButton.surface = Label.Label("CHECK OUT").getSurface()
-        self.surfacesOfItemsShopped = [] #Used for listing items in checkout screen
-        
-        
-        self.title = Label.Label("Main Screen").getSurface()
-        self.titleLocation = (200, 50)
       
         #self._image_surf = pygame.image.load("Images/myimage.jpg").convert()
         #self._image_loc = pygame.Rect(70, 70, 50, 20)
@@ -172,36 +171,20 @@ class Game(events.Event):
                     self.on_minimize()
     def on_loop(self):
         if self._screen == "Main":
-            self.title = Label.Label("Main Screen").getSurface()
             self.buttons = []
             for itemCat in self._itemCategories:
                 self.buttons.append(itemCat.getButton())
-            self.buttons.append(self.checkoutButton)
                 
-        elif self._screen.find("itemCatScreen:") != -1:
-            self.title = Label.Label("Item Category Screen of: " + self._itemCategories[int(self._screen.replace("itemCatScreen:", ""))].name).getSurface()
+                
+        if self._screen.find("itemCatScreen:") != -1:
             itemCatNumber = int(self._screen.replace("itemCatScreen:", ""))
             self.buttons = []
             for item in self._itemCategories[itemCatNumber].items:
                 self.buttons.append(item.getButton())
             self.buttons.append(self.backButton)
             
-        elif self._screen == "checkOut":
-            self.title = Label.Label("Check Out Screen").getSurface()
-            self.buttons = []
-            itemsShopped = []
-            for itemCat in self._itemCategories:
-                for item in itemCat.items:
-                    if item.amount > 0:
-                        itemsShopped.append(item)
-            
-            self.surfacesOfItemsShopped = []
-            for item in itemsShopped:
-                self.surfacesOfItemsShopped.append(Label.Label("Name: " + item.name + "Price: " + str(item.price) + " Amount: " + str(item.amount) + " TotPrice: " + str(item.getTotalPrice())))
-            
     def on_render(self):
         self._display_surf.fill(self._background)
-        self._display_surf.blit(self.title, self.titleLocation)
         for button in self.buttons:
             self._display_surf.blit(button.getSurface(), button.location)
             
@@ -215,9 +198,6 @@ class Game(events.Event):
             for item in self._itemCategories[itemCatNumber].items:
                 self._display_surf.blit(item.getSurface(), item.location)
         
-        elif self._screen == "checkOut":
-            for x in xrange(self.surfacesOfItemsShopped.__len__()):
-                self._display_surf.blit(self.surfacesOfItemsShopped[x].getSurface(), (100, 100 + x *40))
         #self._display_surf.blit(self._image_surf, self._image_loc, pygame.Rect(0, 0, 144, 144))
         #self._display_surf.blit(self._testText.getSurface(), (100,100))
         pygame.display.flip()
